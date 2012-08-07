7003bedcdf6127b958d62d700573b6b7ee95b6a5
Ramon Masek <ramon.masek@fokus.fraunhofer.de>
2012-08-07 12:38:56 +0200

    adjust netview.sh to branch version of jar

--- netview.sh
+++ netview.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # NetView starter script
 #
-java -jar ./netview/target/netview-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar ./netview/target/netview-1.0-SNAPSHOT-plain-jar-with-dependencies.jar
